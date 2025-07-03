236fc7693e2a98a3c80432a8e0bc334bdb3d5f25
Mark Schachner <markj.schachner@gmail.com>
2025-07-02 22:07:48 -0400

    Add earthy theme and textured look

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,10 @@ module.exports = {
         'fairway': '#8fbc8f',
         'rough': '#556b2f',
         'sand': '#f4d03f',
+        'earth-green': '#556b2f',
+        'earth-burlap': '#deb887',
+        'earth-beige': '#f5f5dc',
+        'earth-brown': '#8b4513',
       },
       fontFamily: {
         marker: ['"Permanent Marker"', 'cursive'],
